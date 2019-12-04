04febc647dfb13688b9772e57c8b84d51f1ac540
kyo <phongbinh2511@gmail.com>
2019-12-04 10:14:31 +0800

    Replaced MainPage with DrawingPage.

--- DualViewsDrawingWindowsUniversalApplication/App.xaml.cs
+++ DualViewsDrawingWindowsUniversalApplication/App.xaml.cs
@@ -57,7 +57,7 @@ namespace DualViewsDrawingWindowsUniversalApplication
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(DrawingPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
