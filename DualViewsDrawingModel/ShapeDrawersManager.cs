365cb9f58957db3a48a8ed422b64e85f7a461386
kyo <phongbinh2511@gmail.com>
2019-12-05 21:29:41 +0800

    Utilized ShapeDrawerFactory.CreateShapeDrawer in ShapeDrawersManager.

--- DualViewsDrawingModel/ShapeDrawersManager.cs
+++ DualViewsDrawingModel/ShapeDrawersManager.cs
@@ -27,22 +27,7 @@ namespace DualViewsDrawingModel
         /// </summary>
         public ShapeDrawer CreateShapeDrawer(Point drawingStartingPoint, Point drawingEndingPoint, ShapeDrawerType shapeDrawerType)
         {
-            if ( shapeDrawerType == ShapeDrawerType.None )
-            {
-                throw new ArgumentException(Definitions.ERROR_SHAPE_DRAWER_TYPE_IS_NONE);
-            }
-            if ( shapeDrawerType == ShapeDrawerType.Line )
-            {
-                return new LineDrawer(drawingStartingPoint, drawingEndingPoint);
-            }
-            else if ( shapeDrawerType == ShapeDrawerType.Rectangle )
-            {
-                return new RectangleDrawer(drawingStartingPoint, drawingEndingPoint);
-            }
-            else
-            {
-                throw new ArgumentException(Definitions.ERROR_SHAPE_DRAWER_TYPE_IS_INVALID);
-            }
+            return ShapeDrawerFactory.CreateShapeDrawer(drawingStartingPoint, drawingEndingPoint, shapeDrawerType);
         }
 
         /// <summary>
